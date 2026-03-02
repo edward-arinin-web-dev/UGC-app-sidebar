bc9fdb40d57757e23361df41b44586f3f5c10204
Edward Arinin <edward.arinin@gmail.com>
2026-03-02 11:58:49 +0200

    chore: update vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/ai-app-sidebar/",
+  base: "/UGC-app-sidebar/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
