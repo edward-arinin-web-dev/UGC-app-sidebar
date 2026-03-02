81e13db4deba1ebc0cdf92d38a90da16f447343b
Edward Arinin <edward.arinin@gmail.com>
2026-03-02 11:54:15 +0200

    chore: configure github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/ai-app-sidebar/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
